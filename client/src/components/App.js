bcbb7f216f2177dd202a22ae58ca764191366489
Ricky Thai <rickyvanthai@gmail.com>
2020-11-30 16:14:57 -0800

    Remove some files and add baseUrl config

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,7 +1,11 @@
-import logo from "./logo.svg";
-import "./App.css";
-
 import { useEffect, useState } from "react";
+
+// color pallete
+/*
+Green: #1DB954
+Black: #191414
+White: #fffff
+*/
 
 function App() {
   const [message, setMessage] = useState("");
@@ -20,19 +24,8 @@ function App() {
   };
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>{message}</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>{message}</p>
     </div>
   );
 }
