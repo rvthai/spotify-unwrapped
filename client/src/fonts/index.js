2b5f1477c026fcf11472092c7995f7b92cdbb4b6
Ricky Thai <rickyvanthai@gmail.com>
2021-01-03 20:55:16 -0800

    Delete unecessary fonts and incorporate the rest

---
+++ client/src/fonts/index.js
@@ -0,0 +1,13 @@
+import CircularStdBlack from "./CircularStd-Black.ttf";
+import CircularStdBold from "./CircularStd-Bold.ttf";
+import CircularStdBook from "./CircularStd-Book.ttf";
+import CircularStdMedium from "./CircularStd-Medium.ttf";
+
+const font = {
+  black: CircularStdBlack,
+  bold: CircularStdBold,
+  book: CircularStdBook,
+  medium: CircularStdMedium,
+};
+
+export default font;
--- client/src/styles/GlobalStyle.js
+++ client/src/styles/GlobalStyle.js
@@ -1,17 +1,51 @@
 import { createGlobalStyle } from "styled-components";
-import CircularFont from "../fonts/CircularStd-Book.ttf";
+import font from "../fonts";
 
 const GlobalStyle = createGlobalStyle`
   @font-face {
-    font-family: "Circular";
-    src: url(${CircularFont}) format('truetype');
+    font-family: "Circular Std";
+    src: url(${font.black}) format('truetype');
+    font-style: normal;
+    font-weight: 900;
+  }
+
+  @font-face {
+    font-family: "Circular Std";
+    src: url(${font.bold}) format('truetype');
+    font-style: normal;
+    font-weight: 700;
+  }
+
+  @font-face {
+    font-family: "Circular Std";
+    src: url(${font.medium}) format('truetype');
+    font-style: normal;
+    font-weight: 500;
+  }
+
+  @font-face {
+    font-family: "Circular Std";
+    src: url(${font.book}) format('truetype');
+    font-style: normal;
+    font-weight: 400;
   }
 
   body {
     margin: 0;
     background: #212121;
     color: #fff;
-    font-family: "Circular", "sans-serif";
+    font-family: "Circular Std", "sans-serif";
+    font-weight: 400;
+  }
+
+  button {
+    font-family: "Circular Std";
+    font-weight: 700;
+    color: #fff;
+    text-decoration: none;
+    padding: 15px 50px;
+    background: #1db954;
+    border-radius: 25px;
   }
 `;
 
